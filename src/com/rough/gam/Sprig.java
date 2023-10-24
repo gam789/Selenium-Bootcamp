6a8d5b2d6cce10f5235caeb6de5648b15991befa
Gibi <gibialexmathew@gmail.com>
2023-10-24 21:36:02 +0530

    Removed unused imports

--- src/com/rough/gam/Sprig.java
+++ src/com/rough/gam/Sprig.java
@@ -1,17 +1,10 @@
 package com.rough.gam;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Sprig {
